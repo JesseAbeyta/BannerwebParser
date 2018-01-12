bf7851210dc7410bd7515fe80ef8f76bdedcd19f
BuildTools <unconfigured@null.spigotmc.org>
2018-01-12 18:31:43 -0500

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        String username;
+        String pass;
+        Fetcher fetcher = new Fetcher("https://bannerweb.wpi.edu");
+    }
+}
